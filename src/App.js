0d263b2b77f6760d3b0c77a3007e6202b192c3f7
stormsamurai1 <stormsamurai1@yahoo.com.br>
2020-04-27 14:56:30 -0300

    Adicionando react-icons, react-dnd, react-dnd-html5-backend

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { DndProvider } from 'react-dnd'
+import HTML5Backend from 'react-dnd-html5-backend'
 
 import GlobalStyles from './styles/global'
 
@@ -7,12 +9,11 @@ import Board from './Components/Board/Board'
 
 function App() {
   return (
-    <>
+    <DndProvider backend={HTML5Backend}>
       <Header/>
       <Board/>
       <GlobalStyles/>
-    </>
-    
+    </DndProvider>
   );
 }
 
