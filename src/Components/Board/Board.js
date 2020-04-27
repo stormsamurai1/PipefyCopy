b487549717caebf2045e3b185fc5060dcb1bfc74
stormsamurai1 <stormsamurai1@yahoo.com.br>
2020-04-27 17:01:05 -0300

    Adicionando immer e Draft de mutabilidade

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import produce from 'immer'
 
 import BoardContext from './context'
 
@@ -14,16 +15,21 @@ export default function Board(){
     
     const [lists, setLists] = useState(data)
     
-    function move(from,to){
-        console.log(from, to)   
+    function move(fromList, toList, from, to){
+        setLists(produce(lists, draft =>{
+            const dragged = draft[fromList].cards[from]
+
+            draft[fromList].cards.splice(from,1)
+            draft[toList].cards.splice(to, 0, dragged)
+        }))
     }
 
     return(
         <BoardContext.Provider value={{lists, move}}>
             <Container>
                 {
-                    lists.map((list)=>
-                        <List key={list.title} data={list}/>
+                    lists.map((list,index)=>
+                        <List key={list.title} index={index} data={list}/>
                     )
                 }
             </Container>
