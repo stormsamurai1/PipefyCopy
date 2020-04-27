961fa42a189e0bdb80617b47e6db8e6709698942
stormsamurai1 <stormsamurai1@yahoo.com.br>
2020-04-27 15:46:25 -0300

    Usando funções do DOM para cacular o centro do elemento do drop

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,23 +1,44 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useDrag, useDrop } from 'react-dnd'
 
 import { Container, Label } from './styles'
 
-export default function Card( { data } ){
+export default function Card( { data, index } ){
+
+    const ref = useRef()
 
     const [{ isDragging }, dragRef] = useDrag({
         item: {
-            type: 'CARD'
+            type: 'CARD',
+            index
         },
         collect: (monitor) => ({
             isDragging: monitor.isDragging()
         })
     })
 
-    
+    const [, dropRef] = useDrop({
+        accept: 'CARD',
+        hover(item, monitor){
+            const draggedIndex = item.index
+            const targetIndex = index;
+
+            if(draggedIndex === targetIndex){
+                return;
+            }
+
+            //Function to get properties about HTMl element
+            const targetSize = ref.current.getBoundingClientRect();
+            
+            const targetCenter = (targetSize.bottom - targetSize.top)/2
+            
+        }
+    })   
+
+    dragRef(dropRef(ref))
 
     return(
-        <Container ref={dragRef} isDragging={isDragging}>
+        <Container ref={ref} isDragging={isDragging}>
             <header>
                 {data.labels.map((label)=>
                     <Label key={label} color={label}/>
